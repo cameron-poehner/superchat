a50545bd3503fea5d97c4075de3ed0f22bb210bd
cameron-poehner <cameronpoehner@gmail.com>
2020-11-21 23:22:13 -0800

    Added SignIn, SignOut, ChatRoom Components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import './App.css';
 
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
+
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
 
-firebase.initializeApp({
-    apiKey: "AIzaSyDMCDCxVqvW1INzSL-cA7ULPo3qcS3Jqa4",
-    authDomain: "superchat-4a6cd.firebaseapp.com",
-    databaseURL: "https://superchat-4a6cd.firebaseio.com",
-    projectId: "superchat-4a6cd",
-    storageBucket: "superchat-4a6cd.appspot.com",
-    messagingSenderId: "373829690780",
-    appId: "1:373829690780:web:25b35ab0ebeec6351ad459",
-    measurementId: "G-GB4R8EQGML"
-})
+const config = {
+  apiKey: "AIzaSyDMCDCxVqvW1INzSL-cA7ULPo3qcS3Jqa4",
+  authDomain: "superchat-4a6cd.firebaseapp.com",
+  databaseURL: "https://superchat-4a6cd.firebaseio.com",
+  projectId: "superchat-4a6cd",
+  storageBucket: "superchat-4a6cd.appspot.com",
+  messagingSenderId: "373829690780",
+  appId: "1:373829690780:web:25b35ab0ebeec6351ad459",
+  measurementId: "G-GB4R8EQGML"
+  };
+    if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+    }
+
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 
-const [user] = useAuthState(auth);
+
+
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-
+      <h1>SuperChat</h1>
+      <SignOut />
       </header>
 
       <section>
@@ -44,15 +54,68 @@ function SignIn() {
     auth.signInWithPopup(provider);
   }
   return(
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
+    <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
 
 function ChatRoom() {
+
+  const dummy = useRef();
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limit(25);
 
   const [messages] = useCollectionData(query, {idField: 'id'});
+  
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    });
+    setFormValue('');
+
+    dummy.current.scrollIntoView({ behavior: 'smooth'});
+    
+  }
+
+  return(
+    <>
+    <main>
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      <span ref={dummy}></span>
+    </main>
+    <form onSubmit={sendMessage} >
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+        <button type="submit" disabled={!formValue}>Send</button>
+    </form>
+    </>
+  )
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+ 
+  return( 
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL} alt="" />
+      <p>{text}</p>
+    </div>
+  )
 }
 
 export default App;
